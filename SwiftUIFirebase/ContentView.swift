f2df7c700d8b757f466af27d820a1394075ea622
our-tonghua <t0n9hua@gmail.com>
2024-09-18 12:50:28 -0700

    Initial Commit

---
+++ SwiftUIFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebase
+//
+//  Created by Administrator on 28/06/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Administrator on 28/06/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
