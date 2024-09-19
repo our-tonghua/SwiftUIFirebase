cfe96a673bc57eac6075f62c6c01ee14307e72fc
our-tonghua <t0n9hua@gmail.com>
2024-09-19 12:17:57 -0700

    create second view

---
+++ SwiftUIFirebase/Secondview.swift
@@ -0,0 +1,18 @@
+//
+//  Secondview.swift
+//  SwiftUIFirebase
+//
+//  Created by Administrator on 9/19/24.
+//
+
+import SwiftUI
+
+struct Secondview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Secondview()
+}
